fdc165fd065b76b9637b14a863f3e1b9673dda41
pankaj10664 <pankaj10664@gmail.com>
2018-06-10 16:16:33 +0530

    star printing

---
+++ printing.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Gitar
+
+{
+public static void main(String args[])
+{
+int i,j;
+int k=5;
+
+for(i=1;i<=k;i++)
+{
+for(j=1;j<=i;j++)
+{
+System.out.print("*");
+}
+System.out.println("");
+}
+}
+}
